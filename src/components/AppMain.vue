d15cb183c4ffdf6ad2b7d6e363ac2f1bf102623b
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-18 17:37:26 +0100

    Create Main Shop Section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,7 +2,37 @@
     export default {
         data() {
             return {
-                contentLinkString: 'Content goes here',
+                contentSection: {
+                    contentLinkString: 'Content goes here'
+                },
+
+                shopSection: [
+
+                    {
+                       path: '/public/img/buy-comics-digital-comics.png',
+                       name: 'DIGITAL COMICS'
+                    },
+
+                    {
+                      path:  '/public/img/buy-comics-merchandise.png',
+                      name:  'DC MERCHANDISE'
+                    },
+
+                    {
+                      path:  '/public/img/buy-comics-subscriptions.png',
+                      name:  'SUBSCRIPTION'
+                    },
+
+                    {
+                      path:  '/public/img/buy-comics-shop-locator.png',
+                      name:  'COMIC SHOP LOCATOR'
+                    },
+
+                    {
+                      path:  '/public/img/buy-dc-power-visa.svg',
+                      name:  'DC POWER VISA'
+                    }
+                ] 
             };
         },
         methods: {
@@ -17,9 +47,19 @@
         <section id="content">
             <a href="#">
                 <i class="fa-solid fa-arrow-right-long"></i>
-                    {{ contentLinkString }} 
+                    {{ contentSection.contentLinkString }} 
                 <i class="fa-solid fa-arrow-left-long"></i>
             </a>
+        </section>
+        <section id="shop">
+            <ul>
+                <li v-for="(shop, i) in shopSection">
+                    <div class="shop-icons">
+                        <img :src="shop.path" :alt="shop.name">
+                        <a href="#"> {{ shop.name }}</a>
+                    </div>
+                </li>
+            </ul>
         </section>
     </main>
 </template>
