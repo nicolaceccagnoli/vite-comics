e3373eaf6ce606c30cf5cfe3f8dcd9129c8e7575
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-18 21:05:49 +0100

    Create PseudoElement for Header's Link

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,205 @@
+<script >
+    export default {
+        data() {
+            return {
+
+                currentLink: 0,
+
+                dcComics: [
+                    {
+                       name: 'Charachters'
+                    },
+                    
+                    {
+                        name: 'Comics'
+                    },
+                    {
+                        name: 'Movies'
+                    },
+                    {
+                        name: 'TV'
+                    },
+                    {
+                        name: 'Games'
+                    },
+                    {
+                        name: 'Videos'
+                    },
+                    {
+                        name: 'News'
+                    }
+                ],
+
+                shop: [
+                    {
+                        name: 'Shop DC'
+                    },
+                    {
+                        name: 'Shop DC Collectibles'
+                    }
+                ],
+
+                dc: [
+                    {
+                        name: 'Terms Of Use'
+                    },
+                    {
+                        name: 'Privacy Policy (New)'
+                    },
+                    {
+                        name: 'Ad Choices'
+                    },
+                    {
+                        name: 'Advertising'
+                    },
+                    {
+                        name: 'Jobs'
+                    },
+                    {
+                        name: 'Subscription'
+                    },
+                    {
+                        name: 'Talent Workshops'
+                    },
+                    {
+                        name: 'CPSC Certificates'
+                    },
+                    {
+                        name: 'Ratings'
+                    },
+                    {
+                        name: 'Shop Help'
+                    },
+                    {
+                        name: 'Contact Us'
+                    }
+                ],
+
+                sites: [
+                    {
+                        name: 'DC'
+                    },
+                    {
+                        name: 'MAD Magazine'
+                    },
+                    {
+                        name: 'DC Kids'
+                    },
+                    {
+                        name: 'DC Universe'
+                    },
+                    {
+                        name: 'DC Power Visa'
+                    }
+                ],
+
+                bigLogo: {
+                    path: 'public/img/dc-logo-bg.png',
+                    name: 'DC Logo'
+                },
+
+                button: 'SIGN-UP NOW!',
+
+                socialLinks: [
+                    'public/img/footer-facebook.png',
+                    'public/img/footer-twitter.png',
+                    'public/img/footer-youtube.png',
+                    'public/img/footer-pinterest.png',
+                    'public/img/footer-periscope.png'
+                ]
+
+            };
+        },
+        methods: {
+
+        }
+
+    }
+</script>
+
+<template>
+    <footer>
+        <section id="footer-top">
+            <div id="footer-list-container">
+                <div>
+                    <h4>
+                        DC COMICS
+                    </h4>
+                    <ul>
+                        <li v-for="elem in dcComics">
+                            <a href="#">
+                                {{ elem.name }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>
+                        DC
+                    </h4>
+                    <ul>
+                        <li v-for="elem in dc">
+                            <a href="#">
+                                {{ elem.name }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>
+                        SITES
+                    </h4>
+                    <ul>
+                        <li v-for="elem in sites">
+                            <a href="#">
+                                {{ elem.name }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>
+                        SHOP
+                    </h4>
+                    <ul>
+                        <li v-for="elem in shop">
+                            <a href="#">
+                                {{ elem.name }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div id="big-logo-container">
+                <img :src="bigLogo.path" :alt="bigLogo.name">
+            </div>
+        </section>
+        <section id="footer-bottom"> 
+            <div id="footer-bottom-container">
+                <div>
+                    <button>
+                        {{ button }}
+                    </button>
+                </div>
+
+                <div id="social-links">
+                    <span href="#">
+                        FOLLOW US
+                    </span>
+                    <ul>
+                        <li v-for="socialLink in socialLinks">
+                            <a href="#">
+                                <img :src="socialLink" alt="">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/footer'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,56 @@
+<script >
+    export default {
+        data() {
+            return {
+                navbar: {
+
+                    links: [
+
+                        'CHARACTERS',
+                        'COMICS',
+                        'MOVIES',
+                        'TV',
+                        'GAMES',
+                        'COLLECTIBLES',
+                        'VIDEOS',
+                        'FANS',
+                        'NEWS',
+                        'SHOP'
+
+                    ],
+
+                    logo: '/img/dc-logo.png'
+                    
+            }
+
+
+            };
+        },
+        
+        methods: {
+
+        }
+
+    }
+</script>
+
+<template>
+    <header>
+        <div>
+            <img :src="navbar.logo" alt="">
+        </div>
+        <nav id="menu">
+            <ul id="menu-list">
+                <li v-for="link in navbar.links">
+                    <a href="#">
+                        {{ link }}
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/header';
+</style>
