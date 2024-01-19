cce5c09c4f7f2e8a11466c7f1faaff1ac35ff0b4
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-19 16:23:49 +0100

    Print Comics in Main

---
+++ src/components/singleComic.vue
@@ -0,0 +1,30 @@
+<script >
+    export default {
+        data() {
+            return {
+
+            };
+        },
+        props: {
+            path: String,
+            currentSeries: String,
+        }
+
+    }
+</script>
+
+<template>
+    <div class="single-comic">
+        <div>
+            <img :src="path" :alt="currentSeries">
+        </div>
+
+        <h3>
+            {{ currentSeries }}
+        </h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../assets/scss/main';
+</style>
